2f5905a1cbc7bd717951463553c13738b436c37b
Taylor Flores <developer@taylorflores.com>
2024-05-17 13:06:36 -0500

    started designing style and nav

---
+++ taylorflores.com/src/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='flex justify-between items-center'>
+        <div children className='max-w-.'></div>
+    </div>
+  )
+}
